c9bdf6ec43947202f703560473eaaa92be2ed253
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2015-06-14 22:47:11 -0700

    moved calculator app to here

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,194 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Elizabeth Wei on 3/9/15.
+//  Copyright (c) 2015 Elizabeth Wei. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    private enum Op: Printable {
+        case Operand(Double)
+        case Variable(String)
+        case ConstantOperation(String, () -> Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .Variable(let symbol):
+                    return symbol
+                case .ConstantOperation(let symbol, _):
+                    return symbol
+                case .UnaryOperation(let symbol,_):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String:Op]()
+    
+    private var variableValues = [String: Double]()
+    
+    init() {
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.ConstantOperation("π", {M_PI}))
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷") { $1 / $0 })
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("−") { $1 - $0 })
+        learnOp(Op.UnaryOperation("√", sqrt))
+        learnOp(Op.UnaryOperation("sin", sin))
+        learnOp(Op.UnaryOperation("cos", cos))
+        learnOp(Op.UnaryOperation("ᐩ/-", { -1 * $0}))
+    }
+    
+    func clear() {
+        variableValues = [:]
+        opStack = []
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if(!ops.isEmpty) {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .Variable(let symbol):
+                if let operand = variableValues[symbol] {
+                   return (variableValues[symbol], remainingOps)
+                } else {
+                    return (nil, remainingOps)
+                }
+            case .ConstantOperation(_, let operation):
+                return (operation(), remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    private func getDescription(ops: [Op]) -> (result: String?, remainingOps: [Op]) {
+        if(!ops.isEmpty) {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return ("\(operand)", remainingOps)
+            case .Variable(let symbol):
+                return (symbol, remainingOps)
+            case .ConstantOperation(let operation, _):
+                return (operation, remainingOps)
+            case .UnaryOperation(let operation, _):
+                let op = getDescription(remainingOps)
+                if let operand = op.result {
+                    return (operation + "(" + "\(operand))" + ")", op.remainingOps)
+                }
+                return (operation + "(?)", op.remainingOps)
+            case .BinaryOperation(let operation, _):
+                let op1 = getDescription(remainingOps)
+                if let operand1 = op1.result {
+                    let op2 = getDescription(op1.remainingOps)
+                    if let operand2 = op2.result {
+                        return ("(" + "\(operand2)" + operation + "\(operand1)" + ")", op2.remainingOps)
+                    }
+                    return ("(?" + operation + "\(operand1)" + ")", op2.remainingOps)
+                }
+                let op2 = getDescription(op1.remainingOps)
+                if let operand2 = op2.result {
+                    return ("(" + "\(operand2)" + operation + "?)", op2.remainingOps)
+                }
+                return ("(?" + operation + "?)", op2.remainingOps)
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func getDescription() -> String? {
+        var tempStack = opStack
+        if let description = getDescription(tempStack).result {
+            println("description is " + description)
+            return description
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func pushOperand(operand: String) -> Double? {
+        opStack.append(Op.Variable(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+    
+    func setVariable(symbol: String, value: Double) -> Double? {
+        variableValues[symbol] = value
+        return evaluate()
+    }
+    
+    func getVariableValue() -> Double? {
+        var val = opStack.removeLast() //todo: set variable to expression, not just latest operand
+        switch val {
+        case .Operand(let op):
+            return op
+        default:
+            return nil
+        }
+    }
+    
+    var description: String {
+        get {
+            if let res = getDescription() {
+                return res
+            }
+            else {
+                return " "
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Elizabeth Wei on 3/7/15.
+//  Copyright (c) 2015 Elizabeth Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var prevDisplay: UILabel!
+    
+    var userTypingANum = false //type inference
+    var decPoint = false
+    var brain = CalculatorBrain() //connection from controller to model
+    
+    @IBAction func clear() { //redo this to work with CalculatorBrain
+        displayValue = nil
+        prevDisplay.text = "0="
+        userTypingANum = false
+        decPoint = false
+        brain.clear() 
+    }
+    
+    
+    @IBAction func backspace() {
+        if(userTypingANum) {
+            if(count(display.text!) > 1) {
+                display.text = dropLast(display.text!)
+            }
+            else {
+                display.text = "0"
+                userTypingANum = false
+            }
+        }
+        
+    }
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if(digit == ".") {
+            if(decPoint) {
+                return
+            }
+            else {
+                decPoint = true
+            }
+        }
+        
+        if(userTypingANum) {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            userTypingANum = true
+        }
+    }
+    
+    @IBAction func setVar() {
+        if userTypingANum {
+            enter()
+        }
+        
+        userTypingANum = false
+        decPoint = false
+        
+        if let val = brain.getVariableValue() {
+            if let result = brain.setVariable("M", value: val) {
+                println("calculated result as \(result)")
+                displayValue = result
+            }
+            else {
+                displayValue = nil
+            }
+            prevDisplay.text = brain.description + "="
+        }
+    }
+    
+    
+    @IBAction func pushVar() {
+        brain.pushOperand("M")
+    }
+    
+    @IBAction func invertSign(sender: UIButton) {
+        if userTypingANum {
+            if(displayValue < 0) {
+                display.text = dropFirst(display.text!)
+            }
+            else {
+                display.text = "-" + display.text!
+            }
+        }
+        else {
+            if let operation = sender.currentTitle {
+                if let result = brain.performOperation(operation) {
+                    displayValue = result
+                }
+                else {
+                    displayValue = nil
+                }
+                prevDisplay.text = brain.description + "="
+            }
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userTypingANum {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            }
+            else {
+                displayValue = nil
+            }
+            prevDisplay.text = brain.description + "=";
+        }
+    }
+    
+    @IBAction func enter() {
+        userTypingANum = false
+        decPoint = false
+        if let result = brain.pushOperand(displayValue!) {
+            displayValue = result
+        } else {
+            displayValue = nil
+        }
+        prevDisplay.text = brain.description + "=";
+    }
+    
+    //computed property (value not actually stored)
+    var displayValue: Double? {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            if let val = newValue { //newValue: default name of new value to be set
+                display.text = "\(val)"
+            } else {
+                display.text = " " //used for clear
+            }
+            userTypingANum = false
+            decPoint = false
+        }
+    }
+}
+
+ 
