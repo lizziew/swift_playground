a123bd373822c7b1da7f565eccc608d8f19d0ff8
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2016-04-24 17:56:30 -0400

    settings

--- ElizabethWei/ElizabethWei/SettingsViewController.swift
+++ ElizabethWei/ElizabethWei/SettingsViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    @IBOutlet weak var ExplanationLabel: UILabel!
 
+    @IBOutlet weak var ExplanationLabel2: UILabel!
+    
+    @IBOutlet weak var CreditsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        ExplanationLabel.text = "In the middle tab:\nFIND YOUR FRIENDS in the map! Their pins will show up if you'll be within 50 miles of each other for at least a day.\nTEXT YOUR FRIENDS by tapping on their pin."
+        
+        ExplanationLabel2.text = "In the left tab:\nADD NEW PINS by specifying your location and your data range if you want to be discoverable!"
+        
+        CreditsLabel.text = "Thanks to makeappicon.com and flaticon artists Dave Gandy, Egor Rymyantsev, and Freepik for icons"
     }
     
 }
