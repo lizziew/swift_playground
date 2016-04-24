d478af9d27afc5ff9f4557d1fac727a23757e94f
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2016-04-24 04:54:27 -0400

    Revert "Revert "cloudkit integration""
    This reverts commit 80edf53a2281e6c2d6e0382870122ffd2d2c90f0.

--- ElizabethWei/ElizabethWei/Pin.swift
+++ ElizabethWei/ElizabethWei/Pin.swift
@@ -9,21 +9,25 @@
 import UIKit
 import MapKit
 
-class Pin: NSObject, NSCoding {
-    var location: MKPlacemark
+class Pin: NSObject/*, NSCoding*/ {
+    var name: String
+    var location: CLLocation
     var startDate: NSDate
     var endDate: NSDate
     
-    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
-    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("pins")
+    //static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+   // static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("pins")
     
-    struct PropertyKey {
-        static let locationKey = "name"
-        static let startDateKey = "startDate"
-        static let endDateKey = "endDate"
-    }
+//    struct PropertyKey {
+//        
+//        static let locationKey = "location"
+//        static let startDateKey = "startDate"
+//        static let endDateKey = "endDate"
+//        
+//    }
     
-    init?(location: MKPlacemark, startDate: NSDate, endDate: NSDate) {
+    init?(name: String, location: CLLocation, startDate: NSDate, endDate: NSDate) {
+        self.name = name 
         self.location = location
         self.startDate = startDate
         self.endDate = endDate
@@ -31,16 +35,16 @@ class Pin: NSObject, NSCoding {
         super.init()
     }
     
-    func encodeWithCoder(aCoder: NSCoder) {
-        aCoder.encodeObject(location, forKey: PropertyKey.locationKey)
-        aCoder.encodeObject(startDate, forKey: PropertyKey.startDateKey)
-        aCoder.encodeObject(endDate, forKey: PropertyKey.endDateKey)
-    }
-    
-    required convenience init?(coder aDecoder: NSCoder) {
-        let location = aDecoder.decodeObjectForKey(PropertyKey.locationKey) as! MKPlacemark
-        let startDate = aDecoder.decodeObjectForKey(PropertyKey.startDateKey) as! NSDate
-        let endDate = aDecoder.decodeObjectForKey(PropertyKey.endDateKey) as! NSDate
-        self.init(location: location, startDate: startDate, endDate: endDate)
-    }
+//    func encodeWithCoder(aCoder: NSCoder) {
+//        aCoder.encodeObject(location, forKey: PropertyKey.locationKey)
+//        aCoder.encodeObject(startDate, forKey: PropertyKey.startDateKey)
+//        aCoder.encodeObject(endDate, forKey: PropertyKey.endDateKey)
+//    }
+//    
+//    required convenience init?(coder aDecoder: NSCoder) {
+//        let location = aDecoder.decodeObjectForKey(PropertyKey.locationKey) as! MKPlacemark
+//        let startDate = aDecoder.decodeObjectForKey(PropertyKey.startDateKey) as! NSDate
+//        let endDate = aDecoder.decodeObjectForKey(PropertyKey.endDateKey) as! NSDate
+//        self.init(location: location, startDate: startDate, endDate: endDate)
+//    }
 }
